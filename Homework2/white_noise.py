d13246c6614c664cde2ac734b1c9b8d3fb91f5ec
Jamie Santos <jamiesanto@gmail.com>
2021-11-14 18:14:34 +0100

    HW2: white_noise: clean up comments and add MSD averages

--- Homework2/white_noise.py
+++ Homework2/white_noise.py
@@ -9,11 +9,9 @@ from math import sqrt
 from matplotlib import pyplot as plt 
 nAgents = 50
 nAgentsMSD = 1000
-#nAgentsMSD = 7
 time = 5
 deltaTList = [0.01, 0.05, 0.1]
 nTimeSteps = [int(time/i) for i in deltaTList]
-#nTimeSteps = [3, 4, 5]
 
 # Generate the list of choices for each method
 def generateChoices(numTimeSteps, numAgents):
@@ -28,7 +26,6 @@ plotTitles = ["deltaT = 0.01", "deltaT = 0.05", "deltaT = 0.1"]
 plotColors = ['r','g','b']
 numCases = len(plotTitles)
 plt.figure(figsize=(20,5))
-#trajectories = []
 
 # Generate and plot the trajectories
 whiteNoiseList = generateChoices(nTimeSteps, nAgents)
@@ -58,20 +55,20 @@ for case in range(numCases):
 	deltaT = deltaTList[case]
 	t = [1, 2, 3, 4, 5]
 	trajectory = np.cumsum(whiteNoiseList[case] * sqrt(deltaT),axis = 0)	#x_i+1 = x_i+w_i * sqrt(deltaT)
+
+	# Get the mean square displacements at each time step
 	msdSum = 0
 	msdList = []
-	#for t in range(nTimeSteps[case] - 1):
-	print("CASE##############" + str(t))
 	for t in range(nTimeSteps[case] - 1):
-		#print("trajectory next:" + str(trajectory[t+1,:]))
-		#print("trajectory curr:" + str(trajectory[t,:]))
 		distances = trajectory[t+1,:] - trajectory[0,:]
 		squareDistances = np.square(distances)
 		MSD = (1/nAgentsMSD) * sum(squareDistances)
-		#print("msd:" + str(MSD))
 		msdList.append(MSD)
-	#print(msdList)
-	plt.plot(msdList,linewidth = 0.2, color=plotColors[case])
+
+	print("MSD: " + str(deltaT) + ": " + str(np.average(msdList)))
+
+	# Plot the MSD's
+	plt.plot(msdList,linewidth = 0.5, color=plotColors[case])
 	scale = 1 / deltaT
 	xticks = [0, scale, 2*scale, 3*scale, 4*scale, 5*scale]
 	ticklabels = ['0','1', '2', '3', '4', '5']
