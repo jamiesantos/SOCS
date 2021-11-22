9733247a4c1c87b0f8cc9e56a30e4fc4b0c41ae8
Jamie Santos <jamiesanto@gmail.com>
2021-11-22 23:01:13 +0100

    HW3: run_simulation: clean up code

--- Homework3/run_simulation.py
+++ Homework3/run_simulation.py
@@ -4,6 +4,7 @@ from matplotlib.animation import FuncAnimation
 from patients import Patient
 
 # Initialize parameters/constants
+
 AGENTS = 1000
 INF_RATE = 0.01
 I_INIT = int(round(AGENTS * INF_RATE))
@@ -12,7 +13,8 @@ S_INIT = AGENTS - I_INIT - R_INIT
 TILE_SIZE = 100		# tileSize x tileSize area	
 GAMMA = 0.01		# Recovery rate
 D = 0.8				# Diffusion rate
-beta = 0.6			# Infection rate (varying)
+timeSteps = 2000 	# Experimental value
+beta = 0.6          # Infection rate (varying)
 
 nRecovered = R_INIT
 nInfected = I_INIT
@@ -20,7 +22,10 @@ nInfected = I_INIT
 population = []
 currInfected = 0
 
-# Initialize the population
+#############################
+# Initialize the population #
+#############################
+
 for i in range(AGENTS):
 	# Generate positions
 	x = int(round(np.random.random()*TILE_SIZE))
@@ -33,10 +38,14 @@ for i in range(AGENTS):
 		currInfected += 1
 	population.append(patient)
 
-# Initialize graphics
+########################
+#  Initialize graphics #
+########################
+
 xInit = [p.x for p in population]
 yInit = [p.y for p in population]
 
+# Scatterplot showing mobility of patients
 fig, (ax1, ax2) = plt.subplots(1, 2, sharey=False)
 scatt=ax1.scatter(xInit,yInit,c='blue',s=10)
 ax1.set_yticklabels([])
@@ -45,10 +54,11 @@ ax1.set_xticks([])
 ax1.set_yticks([])
 ax1.set(xlim=(-.1, TILE_SIZE + 0.1), ylim=(-.1, TILE_SIZE + 0.1))
 
+# Plot of SIR statuses over time
 susceptible,=ax2.plot(AGENTS - currInfected,color="blue",label="Susceptible")
 infected,=ax2.plot(currInfected,color="orange",label="Infected")
 recovered,=ax2.plot(0,color="green",label="Recovered")
-ax2.axis([0,1000,0,AGENTS])
+ax2.axis([0,timeSteps,0,AGENTS])
 ax2.legend(handles=[susceptible,infected,recovered])
 ax2.set_xlabel("Time")
 ax2.set_ylabel("People")
@@ -59,8 +69,12 @@ infList = []
 recList = []
 times = []
 
-# Update a single frame
+######################### 
+# Update a single frame #
+#########################
+
 def update(frame,susList,infList,recList,times):
+	# Setting up arrays to pass to matplotlib animation
 	times.append(frame)
 	nSusceptible = 0
 	nInfected = 0
@@ -68,12 +82,14 @@ def update(frame,susList,infList,recList,times):
 	colors = []
 	sizes = [10 for p in population]
 
+	# If patients is infected, possibly infect neighbors
+	# and/or recover. Also keep track of recovered patients.
 	for p in population:		
 		p.update_square(D,TILE_SIZE)
 		if p.infected:
 			nInfected += 1
 
-			# Infect the others nearby
+			# "Expose" the others nearby
 			for other in population:
 				if p.id != other.id and np.random.rand() < beta:
 					other.spread_infection(p)
@@ -81,27 +97,37 @@ def update(frame,susList,infList,recList,times):
 			# Check if patient has recovered
 			if np.random.rand() < GAMMA:
 				p.recover()
+
+		# If the patient was already recovered
 		elif p.recovered:
 			nRecovered += 1
 
 		colors.append(p.plot_color())
 
+	# Add SIR numbers to lists for plotting 
 	nSusceptible = AGENTS - nInfected - nRecovered
 	susList.append(nSusceptible)
 	infList.append(nInfected)
 	recList.append(nRecovered)
 
+	# Plot the scatterplot
 	xPositions = [p.x for p in population]
 	yPositions = [p.y for p in population]
-	offsets=np.array([[p.x for p in population],
-					 [p.y for p in population]])
+	offsets=np.array([xPositions,yPositions])
 	scatt.set_offsets(np.ndarray.transpose(offsets))
 	scatt.set_color(colors)
 	scatt.set_sizes(sizes)
+
+	# Plot the line plots
 	susceptible.set_data(times,susList)
 	infected.set_data(times,infList)
 	recovered.set_data(times,recList)
+
 	return scatt,susceptible,infected,recovered
+
+######################
+# Run the simulation #
+######################
 
 animation = FuncAnimation(fig, update, interval=10,fargs=(susList,infList,recList,times),blit=False)
 plt.show()
